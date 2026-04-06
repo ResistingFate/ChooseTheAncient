87d90c7a8c3fb5c903d6ecf35dde30fb67d07b4f
ResistingFate <49793855+ResistingFate@users.noreply.github.com>
2026-04-06 05:41:47 +0100

    Restructure repo so ChooseTheAncient project folder is the repository root

--- ChooseTheAncient/ChooseTheAncientCode/ChooseTheAncientFlowState.cs
+++
@@ -1,8 +0,0 @@
-namespace ChooseTheAncient.ChooseTheAncientCode;
-
-public sealed class ChooseTheAncientFlowState
-{
-    public HashSet<int> ResolvedActs { get; } = new();
-    public bool FlowInProgress { get; set; }
-    public bool ContinueEnterNextAct { get; set; }
-}
--- ChooseTheAncient/ChooseTheAncientCode/ChooseTheAncientVotingResult.cs
+++
@@ -1,6 +0,0 @@
-namespace ChooseTheAncient.ChooseTheAncientCode;
-
-public readonly record struct ChooseTheAncientVoteResult(
-    int VotedBanIndex,
-    ulong PlayerNetId
-);
